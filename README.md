851bf2652238b5a6e53a146df6089456cab80953
Adeline Bailly <ade.bailly@gmail.com>
2016-11-03 10:53:41 +0100

    Creation README + Minor changes

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# soon
+# Non Linear Time Series Adaptation for Earth Observation
+
+## Demo ##
+
+The current demo is set to work with the [GEE-TSDA dataset](https://github.com/a-bailly/time_series_data).
+Information on data file format are detailed there, each line of text file corresponds to a labeled time series.
+
+## Citation ##
+
+We would like to thanks:
+> Kernel Manifold Alignment for Domain Adaptation. Devis Tuia and Gustau Camps-Valls. PLoS ONE, 2016.
+For making their code available [here](https://github.com/dtuia/KEMA/), on which we based ours.
+
+## License ##
+
+Please refer to [GNU GENERAL PUBLIC LICENSE](https://www.gnu.org/licenses/gpl.html).
--- demo.m
+++ demo.m
@@ -8,16 +8,19 @@ close all;
 
 %% spec
 path = '../time_series_data/';
-name_domA = 'belmanip/fapar.txt';
-name_domB = 'belmanip/lai.txt';
+
+	%% GEE-TSDA
+name_domA = 'gee_tsda/modis_sa_ndvi_8day_2011.txt'; % South America
+%name_domA = 'gee_tsda/modis_na_ndvi_8day_2011.txt'; % North America
+%name_domA = 'gee_tsda/modis_eu_ndvi_8day_2003.txt'; % 2003
+%name_domA = 'gee_tsda/landsat_eu_ndvi_8day_2011.txt'; % Lansat
+%name_domA = 'gee_tsda/modis_eu_lai_4day_2011.txt'; % Lai 4day
+name_domB = 'gee_tsda/modis_eu_ndvi_8day_2011.txt'; % 
+
+	%% BELMANIP
+%name_domA = 'belmanip/fapar.txt'; %
+%name_domB = 'belmanip/fvc.txt'; %
 NN = 5;
-
-%'gee/modis_eu_ndvi_8day_2011.txt'; % 
-
-%'gee/modis_eu_ndvi_8day_2003.txt'; %
-%'gee/modis_na_ndvi_8day_2011.txt'; %
-%'gee/modis_eu_ndvi_16day_2011.txt'; %
-%'gee/landsat_eu_ndvi_8day_2011.txt'; %
 
 addpath(genpath('functions'));
 
@@ -35,21 +38,58 @@ domB = load(s);
 labelsB = domB(:,1);
 domB = domB(:,2:end);
 
-[labels, ~, ic] = unique(labelsA);
+labels = unique(labelsA);
 disp('number of class :'),
 disp(numel(labels));
 
 disp('L1: labels ; L2: Domain A Occurences ; L3: Domain B Occurences');
+	%% Check if each domains contain same classes
+labels = unique(labelsA);
+labelsBb = unique(labelsB);
+if(length(labels) ~= length(labelsBb))
+	new_labels = intersect(labels, labelsBb);
+	for ii=1:length(labels)
+		if(~ismember(labels(ii), new_labels))
+			k = find(labelsA~=labels(ii));
+			domA = domA(k,:);
+			labelsA = labelsA(k);
+		end
+	end
+	for ii=1:length(labelsBb)
+		if(~ismember(labelsBb(ii), new_labels))
+			k = find(labelsB~=labelsBb(ii));
+			domB = domB(k,:);
+			labelsB = labelsB(k);
+		end
+	end
+end
+
+[labels, ~, ic] = unique(labelsA);
 ha = histc(ic, unique(ic));
-[labels, ~, ic] = unique(labelsB);
+[labelsBb, ~, ic] = unique(labelsB);
 hb = histc(ic, unique(ic));
+
 disp([labels'; ha'; hb']);
 
 disp('-----------------------------------')
-disp('-- Classif of A and B using both --')
+run_xp(domA, labelsA, domB, labelsB, NN);
 disp('-----------------------------------')
-ma_predict(domA, labelsA, domB, labelsB, NN);
-disp('-------------------------')
-disp('--Classif of A using B --')
-disp('-------------------------')
-ma_predictAwB(domA, labelsA, domB, labelsB, NN);
+
+fig_mean_profil = 0;
+if(fig_mean_profil == 1)
+	ff = figure(2);
+	x=1:size(domA,2);
+	colormap(jet(numel(labels)));
+	linestyle = {':', '-', '--', '-.'};
+	for ii=1:numel(labels)
+		k = find(labelsA==labels(ii));
+		plot(x, mean(domA(k,:))', 'LineStyle', linestyle{mod(ii,numel(linestyle))+1}), hold on;
+	end
+	leg_str = {'Evergreen', 'Deciduous', 'Shrublands', 'Savannas', 'Grasslands', 'Croplands'}; %
+	leg = columnlegend(3, leg_str);
+	%set(leg, 'position', [0.1,0.05,0.9,0.1] ); % bottom
+	set(leg, 'position', [0.1,0.7,0.9,0.1] ); % top
+	hold off;
+	axis([0 size(domA,2) 0 1]); % put 70 for LAI
+	saveas(ff, '_mean_ts', 'pdf');
+end
